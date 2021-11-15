6c236bab3ea11620f49bffc30e1465e1c66eec82
quality-reaper <dvapixela@gmail.com>
2021-11-15 19:15:00 +0100

    V2
    -Tidied up code

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const rp = require('request-promise'); //npm install request-promise
 const url = 'https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1';
 
+class Product{
+  constructor(name, domestic, price, weight, description){
+    this.name = name;
+    this.domestic = domestic;
+    this.price = price;
+    this.weight = weight;
+    this.description = description;
+  }
+
+  getPrice(){
+    return this.price;
+  }
+
+  print(){
+    console.log(`... `+this.name);
+    console.log(`    Price:`, `$`+this.price.toFixed(1));
+    console.log(`   `, this.description.substring(0,10)+`...`);
+
+    if (this.weight == null)
+      console.log(`    Weight: N/A`);
+    else
+      console.log(`    Weight:`, this.weight+`g`);
+  }
+}
+
 rp(url)
   .then(function(html){
 
-    //sort by domestic, check for json parse
+    //take data
     const doc = JSON.parse(html);
-    var data = doc.sort(function(obj1, obj2){
+    let data = doc.sort(function(obj1, obj2){
       return obj2.domestic - obj1.domestic});
 
-    //sort properly now
-    var group_domestic = {};
-    var group_import = {};
+    //sort in groups, one domestic, other imported
+    let group_domestic = new Array(), group_import = new Array();
+    let domestic_cost = 0, imported_cost = 0;
 
-    //split into two separate objects
-    group_domestic = data.filter(function(obj){
-      return obj.domestic == true;
+    data.forEach(obj=>{
+      let object = new Product(obj.name, obj.domestic, obj.price, obj.weight, obj.description);
+      if (obj.domestic){
+        domestic_cost += object.getPrice();
+        group_domestic.push(object);
+      }
+      else{
+        imported_cost += object.getPrice();
+        group_import.push(object);
+      }
     });
 
-    group_import = data.filter(function(obj){
-      return obj.domestic == false;
-    });
-
-    //sort by alphabet
+    //then sort alphabetically
     group_domestic = group_domestic.sort(function(obj1, obj2){
       let name1 = obj1.name.toUpperCase();
       let name2 = obj2.name.toUpperCase();
@@ -34,49 +62,21 @@ rp(url)
       let name2 = obj2.name.toUpperCase();
       return (name1 < name2) ? -1 : (name1 > name2) ? 1 : 0;
     });
+    
+    //get products amount
+    const domestic_count = group_domestic.length;
+    const imported_count = group_import.length;
 
-    const domestic_count = Object.keys(group_domestic).length;
-    const imported_count = Object.keys(group_import).length;
-
-    let domestic_cost = 0;
-    let imported_cost = 0;
-
-    group_domestic.forEach(obj => {
-      domestic_cost += obj.price;
+    //print elements and rest from requirement
+    console.log(`. Domestic`);
+    group_domestic.forEach(element => {
+      element.print();
     });
 
-    group_import.forEach(obj => {
-      imported_cost += obj.price;  
+    console.log(`. Imported`);
+    group_import.forEach(element => {
+      element.print();
     });
-
-    //print func for both groups
-    function printer(group){
-
-      //check if key has value, if true domestic, else import
-      let check = false;
-      group.forEach(obj => {
-        check = obj.domestic;
-      });
-
-      if(check)
-        console.log(`.`, `Domestic`);
-      else
-        console.log(`.`, `Imported`);
-
-      group.forEach(obj => {
-        console.log(`...`, obj.name);
-        console.log(`    Price:`, `$`+obj.price.toFixed(1));
-        console.log(`   `, obj.description.substring(0,10)+`...`);
-  
-        if (obj.weight == null)
-          console.log(`    Weight: N/A`);
-        else
-          console.log(`    Weight:`, obj.weight+`g`);
-      });
-    }
-
-    printer(group_domestic);
-    printer(group_import);
 
     console.log(`Domestic cost:`, `$`+domestic_cost.toFixed(1));
     console.log(`Imported cost:`, `$`+imported_cost.toFixed(1));
